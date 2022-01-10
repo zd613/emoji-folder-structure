d65999c814f86c67cf19154f3562a5508a179e4e
ame <25921047+zd613@users.noreply.github.com>
2022-01-10 12:53:41 +0900

    フォルダー選択ダイアログが2つ開いていたので修正

--- src/components/FolderSelectArea.vue
+++ src/components/FolderSelectArea.vue
@@ -1,9 +1,5 @@
 <template>
-  <button
-    type="button"
-    @click="$emit('click', $event)"
-    class="bg-white p-2 text-xl font-semibold rounded"
-  >
+  <button type="button" class="bg-white p-2 text-xl font-semibold rounded">
     フォルダーを選択する
   </button>
 </template>
